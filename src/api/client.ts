536a009a38867b27861212b1a093edf7615025a4
Aashu-king <01kingrajput01@gmail.com>
2025-07-25 07:12:48 +0530

    redeploy

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../utils/auth';
 
 
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://fileuploadbackend-0fbn.onrender.com/api/v1';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api/v1';
+export const API_BASE_URL = 'https://fileuploadbackend-0fbn.onrender.com/api/v1';
 
 export const IMAGE_MIME_TYPES = [
   'image/jpeg',
